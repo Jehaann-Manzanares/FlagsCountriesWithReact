cb47097e2532d79ceeb8bb389fb2f160b4b483c2
Jehaann Manzanares <jehaann07carlos@gmail.com>
2020-06-23 22:25:53 -0600

    Updates

---
+++ precache-manifest.7fa19e14769e2e2a5b8616bde6ecacce.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "034b05fd05dd495d51ba2914ceb128d7",
+    "url": "/FlagsCountriesWithReact/index.html"
+  },
+  {
+    "revision": "48f9ef189a0b0b84fc4c",
+    "url": "/FlagsCountriesWithReact/static/css/main.f5998cda.chunk.css"
+  },
+  {
+    "revision": "93f1af9a6ce86447c172",
+    "url": "/FlagsCountriesWithReact/static/js/2.1603fdf7.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/FlagsCountriesWithReact/static/js/2.1603fdf7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48f9ef189a0b0b84fc4c",
+    "url": "/FlagsCountriesWithReact/static/js/main.26d2346f.chunk.js"
+  },
+  {
+    "revision": "ad40ec82f046f7d51746",
+    "url": "/FlagsCountriesWithReact/static/js/runtime-main.8b5ea4a5.js"
+  }
+]);
